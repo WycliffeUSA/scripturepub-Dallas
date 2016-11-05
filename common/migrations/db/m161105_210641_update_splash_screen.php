3a784926e038022b8a00b5657a951e00661d4b4e
Chris Hubbard <chris@thehubbards.org>
2016-11-05 16:12:45 -0500

    Update carousel images

---
+++ common/migrations/db/m161105_210641_update_splash_screen.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\db\Migration;
+
+class m161105_210641_update_splash_screen extends Migration
+{
+    public function up()
+    {
+        $this->update('{{%widget_carousel_item}}', [
+            'carousel_id'=>1,
+            'base_url' => Yii::getAlias('@frontendUrl'),
+            'path'=>'img/scripture_app_in_hand.jpg',
+            'type'=>'image/jpeg',
+            'url'=>'/',
+            'status'=>1
+        ]);
+
+        $this->insert('{{%widget_carousel_item}}', [
+            'carousel_id'=>1,
+            'base_url' => Yii::getAlias('@frontendUrl'),
+            'path'=>'img/dictionary_app_in_hand.jpg',
+            'type'=>'image/jpeg',
+            'url'=>'/',
+            'status'=>1
+        ]);
+
+        $this->insert('{{%widget_carousel_item}}', [
+            'carousel_id'=>1,
+            'base_url' => Yii::getAlias('@frontendUrl'),
+            'path'=>'img/reading_app_in_hand.jpg',
+            'type'=>'image/jpeg',
+            'url'=>'/',
+            'status'=>1
+        ]);
+
+    }
+
+    public function down()
+    {
+        echo "m161105_210641_update_splash_screen cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
